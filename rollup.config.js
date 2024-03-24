743d4992d02afae4854c0886a84ca75ae705514a
Zsolt Tövis <tovis.zsolt@gmail.com>
2024-03-24 04:15:36 +0100

    cleaning

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,20 @@ const commonjs = require('@rollup/plugin-commonjs');
 const { terser } = require('rollup-plugin-terser');
 const copy = require('rollup-plugin-copy');
 
-const buildFolder = './dist';
-const configTerser = { format: { comments: false } };
-const configCopy = { targets: [{ src: 'src/index.d.ts', dest: buildFolder }] };
-const plugins = [commonjs(), terser(configTerser), copy(configCopy)];
+const dist = './dist';
+const config = {
+  terser: { format: { comments: false } },
+  copy: { targets: [{ src: 'src/index.d.ts', dest: dist }] },
+};
+const plugins = [commonjs(), copy(config.copy)];
 
 module.exports = [
   {
     input: 'src/index.js',
     output: [
-      { name: 'index-mjs', file: `${buildFolder}/index.js`, format: 'es' },
-      { name: 'index-cjs', file: `${buildFolder}/index.cjs`, format: 'cjs' },
-      { name: 'Base64', file: `${buildFolder}/browser.js`, format: 'iife' },
+      { file: `${dist}/index.js`, format: 'es' },
+      { file: `${dist}/index.cjs`, format: 'cjs' },
+      { file: `${dist}/browser.js`, format: 'iife', name: 'Base64', plugins: [terser(config.terser)] },
     ],
     plugins,
   },
