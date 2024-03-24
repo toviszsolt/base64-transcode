2b96b7594c2f5a62d6d47e04e287bd7039b2fb7f
Zsolt Tövis <tovis.zsolt@gmail.com>
2024-03-24 04:14:23 +0100

    improvements

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
 declare module 'base64-transcode' {
-  export function decode(base64: string, binary?: boolean): string | Uint8Array;
-  export function encode(str: string, binary?: boolean): string;
+  export function encode(input: string | Uint8Array | Buffer): string;
+  export function decode(base64: string, toBuffer?: boolean): string | Uint8Array | Buffer;
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-const decode = (base64, binary = false) => {
-  const binString = atob(base64);
-  const bytes = Uint8Array.from(binString, (m) => m.codePointAt(0));
-  return binary ? bytes : new TextDecoder().decode(bytes);
-};
-
-const encode = (str, binary = false) => {
-  const bytes = binary ? str : new TextEncoder().encode(str);
+/**
+ * Encode a String or ArrayBuffer to Base64.
+ *
+ * @example
+ * const encoded = Base64.encode('Hello World!');
+ * @param {string|Uint8Array|Buffer} input String or ArrayBuffer to be encoded
+ * @returns {string} Base64 encoded string
+ */
+const encode = (input) => {
+  const bytes = ArrayBuffer.isView(input) ? input : new TextEncoder().encode(input);
   const binString = String.fromCodePoint(...bytes);
   return btoa(binString);
 };
 
-module.exports = {
-  encode,
-  decode,
+/**
+ * Decode Base64 to String or ArrayBuffer
+ *
+ * @example
+ * const decoded = Base64.decode('SGVsbG8gV29ybGQh!');
+ * const decoded = Base64.decode('SGVsbG8gV29ybGQh!', true);
+ * @param {string} base64 Base64 string to be decoded
+ * @param {boolean} toBuffer Return ArrayBuffer instead of String. Default: false.
+ * @returns {string|Uint8Array|Buffer} Encoded string or ArrayBuffer
+ */
+const decode = (base64, toBuffer = false) => {
+  const binString = atob(base64);
+  const bytes = Uint8Array.from(binString, (m) => m.codePointAt(0));
+  return toBuffer ? (typeof Buffer === 'function' ? Buffer.from(bytes) : bytes) : new TextDecoder().decode(bytes);
 };
+
+module.exports = { encode, decode };
